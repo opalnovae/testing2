7b40d0057188f4b23ae97f8e9c3d4b4328bbe86b
opalnovae <jnbostow@gmail.com>
2017-10-27 15:58:03 -0700

    todo list ticket1

--- testing2.js
+++ testing2.js
@@ -10,13 +10,10 @@ returnChildren(checkElement, passFunct);
 
 //takes an element as an argument and console.log's its immediate children text.
 function returnChildren(checkElement, passFunct) {
-    (checkElement.childNodes).forEach(function (elem) {
-        if(elem.childNodes.length === 0) {
-            passFunct(elem);
-        }
-        else {
-            passFunct(elem);
-            returnChildren(elem, passFunct);
+    checkElement.childNodes.forEach(function (elem) {
+        passFunct(elem);
+        if(elem.childNodes.length > 0) {
+            returnChildren(elem,passFunct);
         }
     });
 
